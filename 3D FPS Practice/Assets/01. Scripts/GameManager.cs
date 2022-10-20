afc582e6f7a04e01c3999cae7388b59415687095
kacard123 <99639271+kacard123@users.noreply.github.com>
2022-10-20 16:14:01 +0900

    20221020-1

---
+++ 3D FPS Practice/Assets/01. Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    
+    public bool isGameover = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
