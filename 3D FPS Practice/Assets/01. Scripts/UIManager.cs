910efe31d9c3c882e4d548d86f58316a4223fc90
kacard123 <99639271+kacard123@users.noreply.github.com>
2022-10-31 16:38:27 +0900

    20221031-1

---
+++ 3D FPS Practice/Assets/01. Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    
+    // Update is called once per frame
+    public void UpdateAmmoText(int magAmmo, int ammoRemain)
+    {
+        
+    }
+}
